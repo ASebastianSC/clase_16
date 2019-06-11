2ab9a58045d8186947c6c75e2b4f5b1501f5cae0
Sebastian Subizar <mahoushonen@99.200-57-1.bestelclientes.com.mx>
2019-06-10 21:48:52 -0500

    thirth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 function rootHandler (request, response) {
+    console.log('root handler')
     response.set ('Content-Type', 'text/plain')
     response.status(201)
     response.send('<h1>Hello World</h1')
@@ -13,5 +14,12 @@ function rootHandler (request, response) {
 app.use('/assets', express.static('static'))
 
 app.all('/', rootHandler)
+
+function authenticationHandler (request, response) {
+    console.log(request.body)
+    response.send('auth')
+}
+app.use(express.json())
+app.post('/auth', authenticationHandler)
 
 app.listen(9000)
