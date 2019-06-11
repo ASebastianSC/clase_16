42bc840f8dc7b452b0426ca3f91834c728f69745
Sebastian Subizar <mahoushonen@99.200-57-1.bestelclientes.com.mx>
2019-06-10 21:21:38 -0500

    second

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
 const app = express()
-
-const serveMiddleware = serveStatic(path.resolve(__dirname, 'static'))
-
 
 function rootHandler (request, response) {
     response.set ('Content-Type', 'text/plain')
@@ -12,17 +7,11 @@ function rootHandler (request, response) {
     response.send('<h1>Hello World</h1')
 }
 
-function searchHandler (request, response) {
-    response.send(request.query.q)
-
-}
 //dependencia de desarrollo: brindan utileria para hacer trabajo más agil
 // dependencia de producción: necesaria y parte del producto
 
-app.use('/assets', serveMiddleware)
+app.use('/assets', express.static('static'))
 
 app.all('/', rootHandler)
-
-app.get('/search', searchHandler)
 
 app.listen(9000)
